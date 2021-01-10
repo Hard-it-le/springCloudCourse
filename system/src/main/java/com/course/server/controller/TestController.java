7a628e43796666dc27cff67122375f20666c718d
yu183 <yjl>
2021-01-10 16:59:13 +0800

    新增server公共服务模块

--- system/src/main/java/com/course/server/controller/TestController.java
+++ system/src/main/java/com/course/server/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.course.system.controller;
+package com.course.server.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
