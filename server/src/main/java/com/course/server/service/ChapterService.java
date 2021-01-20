1cd5df414ec399d36764be683b0adde9bbb1a46f
yu183 <yjl>
2021-01-20 11:54:34 +0800

    1.增加分页组件pagination.vue 2.增加mybatis的分页插件 3.chapter页面使用分页组件进行初始化页面条数, 4.添加分页Dto

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -1,9 +1,7 @@
 package com.course.server.service;
 
-import java.util.List;
+import com.course.server.dto.PageDto;
 
-public interface ChapterService  {
-
-
-      List  list();
+public interface ChapterService {
+      void list(PageDto pageDto);
 }
--- server/src/main/java/com/course/server/service/impl/ChapterServiceImpl.java
+++ server/src/main/java/com/course/server/service/impl/ChapterServiceImpl.java
@@ -3,8 +3,11 @@ package com.course.server.service.impl;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.course.server.service.ChapterService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -19,18 +22,21 @@ public class ChapterServiceImpl implements ChapterService {
     private ChapterMapper chapterMapper;
 
     @Override
-    public List list() {
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
-        List<ChapterDto> chapterDtos = new ArrayList<ChapterDto>();
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
         for (int i = 0; i < chapterList.size(); i++) {
             Chapter chapter = chapterList.get(i);
             ChapterDto chapterDto = new ChapterDto();
-            BeanUtils.copyProperties(chapter,chapterDto);
-            chapterDtos.add(chapterDto);
+            BeanUtils.copyProperties(chapter, chapterDto);
+            chapterDtoList.add(chapterDto);
         }
-
-
-        return chapterDtos;
+            pageDto.setList(chapterDtoList);
     }
+
+
 }
